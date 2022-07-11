e6f15093d8b67c216b00b202950e56db726ac599
Mayur Ramdham <93993478+mayurramdham@users.noreply.github.com>
2022-07-11 21:28:23 +0530

    Add files via upload

---
+++ todo-list-master/fireConfig.js
@@ -0,0 +1,14 @@
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDlmipkC-4iHDrR4KKLWLNBidU-zqaTo-k",
+//     authDomain: "todo-list-e8c1e.firebaseapp.com",
+//     projectId: "todo-list-e8c1e",
+//     storageBucket: "todo-list-e8c1e.appspot.com",
+//     messagingSenderId: "28543758262",
+//     appId: "1:28543758262:web:a31ee4456a4ca6b55d1b17",
+//     measurementId: "G-4CWZ7MVHG1"
+//   };
+  
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics();
